c073803b849a269094f220e797d8f1934d5c7856
Mate Matiovics <dontirikk@gmail.com>
2017-06-07 16:11:34 +0200

    b6

--- src/Greet.java
+++ src/Greet.java
@@ -2,7 +2,7 @@
  * Created by Adam on 2017. 06. 07..
  */
 public class Greet {
-  public static void greet() {
-    System.out.println("Helo");
+  public static void greet(String parameter) {
+    System.out.printf("Greetings for the %s", parameter);
   }
 }
