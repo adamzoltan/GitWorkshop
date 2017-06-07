82a0bf4e587d6332f1f9236ded0e04675210fc15
Adam Zoltan Nagy <adamzoltan.nagy@gmail.com>
2017-06-07 15:56:00 +0200

    a5

--- src/Greet.java
+++ src/Greet.java
@@ -2,7 +2,8 @@
  * Created by Adam on 2017. 06. 07..
  */
 public class Greet {
-  public static void greet() {
-    System.out.println("Helo");
+  public static void greet(String name1, String name2) {
+    System.out.println("Hello" + name1);
+    System.out.println("Hello" + name2);
   }
 }
